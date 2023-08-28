310e31e01da19088fe0cb91d350516f4bd630080
Altimar <128001062+AltJuniior@users.noreply.github.com>
2023-08-28 11:50:25 -0300

    Co-authored-by: Altimar <AltJuniior@users.noreply.github.com> Co-authored-by: Eduardo Santos Lopes <eduardo51438876@aluno.ba.senac.br>

--- js/form.js
+++ js/form.js
@@ -1,16 +1,30 @@
-(function () {
-    'use strict'
-    const forms = document.querySelectorAll('.requires-validation')
-    Array.from(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-    
-          form.classList.add('was-validated')
-        }, false)
-      })
-    })()
-    
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtenha referências para as seções de opções de estilo de cabelo
+  const longStylesSection = document.getElementById('longStyles');
+  const mediumStylesSection = document.getElementById('mediumStyles');
+  const shortStylesSection = document.getElementById('shortStyles');
+
+  // Obtenha referências para os botões de rádio de comprimento de cabelo
+  const longHairRadio = document.getElementById('long');
+  const mediumHairRadio = document.getElementById('average');
+  const shortHairRadio = document.getElementById('short');
+
+  // Adicione ouvintes de evento para os botões de rádio de comprimento de cabelo
+  longHairRadio.addEventListener('change', function () {
+    longStylesSection.classList.remove('d-none');
+    mediumStylesSection.classList.add('d-none');
+    shortStylesSection.classList.add('d-none');
+  });
+
+  mediumHairRadio.addEventListener('change', function () {
+    longStylesSection.classList.add('d-none');
+    mediumStylesSection.classList.remove('d-none');
+    shortStylesSection.classList.add('d-none');
+  });
+
+  shortHairRadio.addEventListener('change', function () {
+    longStylesSection.classList.add('d-none');
+    mediumStylesSection.classList.add('d-none');
+    shortStylesSection.classList.remove('d-none');
+  });
+});
